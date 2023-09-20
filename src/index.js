3ec33e171cfec8eef8f54176ad0b7c7c6d62192a
gibonDavid <gibon.david@live.fr>
2023-09-20 09:17:37 +0200

    first commit

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import _ from 'lodash';
+import '../src/scss/styles.scss'; // Import scss
+function component() {
+  const element = document.createElement('div');
+  element.classList.add('container');
+  element.innerHTML = _.join(['Hello', 'OnePoint'], ' ');
+  return element;
+}
+document.body.appendChild(component());
